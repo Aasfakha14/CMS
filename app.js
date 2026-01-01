809ff765830674489703e1c2faf34aa3ea5adcbd
Aasfakhan14 <aasfakhan671@gmail.com>
2026-01-01 11:01:38 +0545

    editpage added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { blogs } = require("./model/index");
+const { where } = require("sequelize");
 const app = express()
 app.set("view engine","ejs")
 
@@ -39,8 +40,53 @@ await blogs.create({
     
 )
 
-app.get("/singlepage",(req,res)=>{
-    res.render("Singlepage")
+app.get("/singlepage/:id",async(req,res)=>{
+    const id = req.params.id
+    const data = await blogs.findAll({
+        where:{
+            id:id
+        }
+})
+    
+    res.render("Singlepage",{data:data})
+})
+app.get("/delete/:id",async(req,res)=>{
+    const id= req.params.id
+    await blogs.destroy({
+        where:{
+            id:id
+        }
+    })
+    res.redirect("/")
+})
+app.post("/editPage/:id",async(req,res)=>{
+    const id= req.params.id
+    const title= req.body.title
+    const description= req.body.description
+    const subTitle= req.body.subTitle
+
+    await blogs.update({
+        title:title,
+        description:description,
+        subTitle:subTitle
+    }, {
+        where: {
+            id:id
+        }
+    })
+    res.redirect("/")
+})
+
+
+
+app.get("/edit/:id",async(req,res)=>{
+    const id = req.params.id
+  const data = await blogs.findAll({
+        where: {
+            id:id
+        }
+    })
+    res.render("editpage", {data:data})
 })
 
 app.listen(3000,function(){
